2b12a8518892a33cb5beaa5ff91fe0b0f920303d
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-27 18:10:31 +0530

    Add project files.

---
+++ ProductList/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductList
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Product Details:{Id} {Name} {Price}";  
+        }
+    }
+    
+}
---
+++ ProductList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductList
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+          
+
+            List<Product> list3 = new List<Product>();
+            list3.Add(new Product { Id = 1, Name = "Laptop", Price = 25000 });
+           
+
+            // or
+
+            List<Product> products = new List<Product>()
+            {
+                new Product { Id = 1, Name = "Laptop", Price = 25000 },
+                new Product { Id = 2, Name = "Laptop", Price = 25000 },
+                new Product { Id = 3, Name = "Laptop", Price = 25000 },
+               
+            };
+            //Insert, CopyTo, Sort, Reverse , IndexOf, Count, Capacity
+            foreach (Product p in products)
+            {
+                Console.WriteLine(p);
+            }
+          
+        }
+    }
+
+   
+}
